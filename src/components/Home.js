34ccc5799f7a34692f02fc18d6e83ad1783a8714
Nishtha Jain <127538617+Nishtha-Jain-1119@users.noreply.github.com>
2023-11-24 20:06:30 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import Header from './Header'
+import Code from './Code'
+import Result from './Result'
+
+export default function Home() {
+  return (
+    <>
+          <Header />
+          <Code />
+          <Result />
+    </>
+  )
+}
